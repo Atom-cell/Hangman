009660ab41d9299ad98e31dbcfac14f8515ddcd3
sani.usnain <sani.usnain@onescreensolutions.com>
2024-01-31 16:42:23 +0500

    added tailwind in gallows

--- src/App.tsx
+++ src/App.tsx
@@ -101,34 +101,31 @@ function App() {
 	}, [usedLetters, correctLetters, checkLetter]);
 
 	return (
-		<div className='font-mono w-2/4 h-full flex flex-col justify-center items-center'>
-			<h1 className='text-white text-4xl'>HANGMAN</h1>
+		<div className='font-mono flex flex-col justify-center items-center'>
+			<h2 className='text-white text-4xl mb-5'>{message}</h2>
+			{/* <h1 className='text-white text-4xl'>HANGMAN</h1> */}
 			<Gallow lives={lives} />
-			<h2>{message}</h2>
 
-			{loading ? (
-				<Loader />
-			) : (
-				<h1 style={{ display: 'flex' }}>
-					{word.split('').map((word: string, index: number) => (
-						<p
-							key={index}
-							style={usedLetters.includes(word) ? { color: 'black' } : {}}
-							className='word'
-						>
-							{word}
-						</p>
-					))}
-				</h1>
-			)}
+			<div className='mb-10 mt-7'>
+				{loading ? (
+					<Loader />
+				) : (
+					<h1 style={{ display: 'flex' }}>
+						{'word'.split('').map((word: string, index: number) => (
+							<p
+								key={index}
+								className={`border-b-4 border-white w-10 text-center mx-3 text-3xl ${
+									usedLetters.includes(word) ? 'text-white' : 'text-gray-900'
+								}`}
+							>
+								{word}
+							</p>
+						))}
+					</h1>
+				)}
+			</div>
 
-			<button
-				className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex justify-center items-center'
-				onClick={() => reset()}
-			>
-				<FaRedo className='mr-2' /> RESET
-			</button>
-			<div className='flex flex-wrap'>
+			<div className='flex flex-wrap md:w-full lg:w-2/4 px-2'>
 				{letters.map((letter: string) => {
 					return (
 						<div className='w-11 border-solid mx-4 my-2'>
@@ -136,9 +133,9 @@ function App() {
 								key={letter}
 								className={`${
 									usedLetters.includes(letter)
-										? 'bg-gray-800'
-										: 'bg-green-800 hover:bg-green-700'
-								} text-white py-4 px-6 rounded border-solid border-2 transition-transform duration-300 ease-in-out transform hover:translate-y-px active:translate-y-0`}
+										? 'bg-gray-400 text-black'
+										: 'bg-gray-800 hover:bg-gray-600 text-white'
+								}  py-4 px-6 rounded border-solid border-2 transition-transform duration-300 ease-in-out transform hover:translate-y-px active:translate-y-0`}
 								disabled={usedLetters.includes(letter)}
 								onClick={() => checkLetter(letter)}
 							>
@@ -148,6 +145,13 @@ function App() {
 					);
 				})}
 			</div>
+
+			<button
+				className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex justify-center items-center'
+				onClick={() => reset()}
+			>
+				<FaRedo className='mr-2' /> RESET
+			</button>
 		</div>
 	);
 }
--- src/Gallow.tsx
+++ src/Gallow.tsx
@@ -2,16 +2,22 @@ import React from "react";
 
 
 const Gallow = ({lives}:{lives:number[]}) => {
+	const headClass = 'rounded-full border-4 w-12 h-12 absolute left-12 top-11';
+	const bodyClass = 'border-l-4 h-12 absolute left-[71px] top-[92px]';
+	const leftArm = 'border-l-4 h-8 absolute left-[59px] top-[97px] rotate-45';
+	const rightArm = 'border-l-4 h-8 absolute left-[83px] top-[97px] rotate-[313deg]';
+	const leftLeg = 'border-l-4 h-8 absolute left-[59px] top-[135px] rotate-45';
+	const rightLeg = 'border-l-4 h-8 absolute left-[83px] top-[135px] rotate-[313deg]';
 	return (
-		<div className='gallow'>
-			<div className='rope'></div>
-			<div className={lives.length >= 1 ? 'head' : ''}></div>
-			<div className={lives.length >= 2 ? 'body' : ''}></div>
-			<div className={lives.length >= 3 ? 'left_arm' : ''}></div>
-			<div className={lives.length >= 4 ? 'right_arm' : ''}></div>
-			<div className={lives.length >= 5 ? 'left_leg' : ''}></div>
-			<div className={lives.length >= 6 ? 'right_leg' : ''}></div>
-			<div className='base'></div>
+		<div className='border-l-4 border-t-4 h-52 w-20 relative'>
+			<div className='border-r-4 h-12 absolute right-0'></div>
+			<div className={lives.length >= 1 ? headClass : ''}></div>
+			<div className={lives.length >= 2 ? bodyClass : ''}></div>
+			<div className={lives.length >= 3 ? leftArm : ''}></div>
+			<div className={lives.length >= 4 ? rightArm : ''}></div>
+			<div className={lives.length >= 5 ? leftLeg : ''}></div>
+			<div className={lives.length >= 6 ? rightLeg : ''}></div>
+			<div className='border-t-4 w-12 absolute bottom-0'></div>
 		</div>
 	);
 };
